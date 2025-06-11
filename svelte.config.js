8daa077d23bf73f3ab61ed0e20324bc4f561d544
zouziflex <zouziflex@gmail.com>
2025-06-11 17:52:03 +0200

    deploy test 1

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-const config = { kit: { adapter: adapter() } };
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
 
 export default config;
